9ab9a4bd09a40879c7915707811b419a31fb1229
Hyungs0703 <hyungs0703@naver.com>
2024-12-14 14:51:06 +0900

    git init

---
+++ src/main/java/com/example/sesacsemi2/SesacSemi2Application.java
@@ -0,0 +1,13 @@
+package com.example.sesacsemi2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SesacSemi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SesacSemi2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sesacsemi2/SesacSemi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sesacsemi2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SesacSemi2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
