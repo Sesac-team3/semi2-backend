57a31aa9a7adbf9691083f121e48a2797c856613
Hyungs0703 <hyungs0703@naver.com>
2024-12-18 17:08:47 +0900

    제목: user 권한 확인 및 데이터 확인

--- src/main/java/com/example/sesacsemi2/global/jwt/UserDetailsServiceImpl.java
+++ src/main/java/com/example/sesacsemi2/global/jwt/UserDetailsServiceImpl.java
@@ -1,8 +1,25 @@
 package com.example.sesacsemi2.global.jwt;
 
+import com.example.sesacsemi2.global.exception.ErrorCode;
+import com.example.sesacsemi2.global.exception.GlobalException;
+import com.example.sesacsemi2.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import com.example.sesacsemi2.user.entity.User;
 
 @Service
-public class UserDetailsServiceImpl {
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
 
+	private final UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByUsername(username)
+			.orElseThrow(() -> new GlobalException(ErrorCode.NOT_FOUND_USERNAME));
+		return new UserDetailsImpl(user);
+	}
 }
