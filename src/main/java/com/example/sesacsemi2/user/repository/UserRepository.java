a454625e60c3a9a6c2fd888646e7d849d5761047
Hyungs0703 <hyungs0703@naver.com>
2024-12-18 17:08:47 +0900

    제목: user 권한 확인 및 데이터 확인

---
+++ src/main/java/com/example/sesacsemi2/user/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.sesacsemi2.user.repository;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.sesacsemi2.user.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	Optional<User> findByUsername(String username);
+}
