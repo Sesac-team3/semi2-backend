83bd73e6689622fa86b8c59fe43f539978d53e28
Hyungs0703 <hyungs0703@naver.com>
2024-12-19 11:56:24 +0900

    제목: 회원가입 기능구현

--- src/main/java/com/example/sesacsemi2/user/controller/AuthController.java
+++ src/main/java/com/example/sesacsemi2/user/controller/AuthController.java
@@ -1,7 +1,15 @@
 package com.example.sesacsemi2.user.controller;
 
+import com.example.sesacsemi2.global.dto.CommonResponseDto;
+import com.example.sesacsemi2.user.dto.SignUpRequestDto;
 import com.example.sesacsemi2.user.repository.UserRepository;
+import com.example.sesacsemi2.user.service.UserServiceImpl;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +19,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
 	private final UserRepository userRepository;
+	private final UserServiceImpl userService;
 
 
+	@PostMapping("/signup")
+	public ResponseEntity<CommonResponseDto<Void>> signup(
+		@RequestBody @Valid SignUpRequestDto signUpRequestDto) {
 
+		userService.signup(signUpRequestDto);
+
+		return ResponseEntity.ok().body(new CommonResponseDto<>(
+			HttpStatus.CONTINUE.value(), "회원가입이 완료되었습니다", null));
+	}
 
 }
---
+++ src/main/java/com/example/sesacsemi2/user/service/UserServiceImpl.java
@@ -0,0 +1,9 @@
+package com.example.sesacsemi2.user.service;
+
+
+import com.example.sesacsemi2.user.dto.SignUpRequestDto;
+
+public interface UserServiceImpl {
+
+	void signup(SignUpRequestDto signUpRequestDto);
+}
